0f4b5c8310b477445f7e140b5e50b50a4dc45750
arslene-kabinda <arskabinda@gmail.com>
2022-01-05 16:19:15 +0100

    feat:add connection with local database and atlas database

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello from the server side ");
+  res
+    .status(200)
+    .json({ message: "hello from the server side ", app: "lizzy" });
+});
+app.post("/", (req, res) => {
+  res.send("you can post to this endpoint");
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const app = require("./app");
-const port = 4000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const DataBase = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DataBase, { useNewUrlParser: true }).then((connection) => {
+  console.log("Database connection successful!");
+});
+
+const port = 4000;
 app.listen(port, () => {
   console.log(`app is running on port ${port}...`);
 });

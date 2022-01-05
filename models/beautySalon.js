48c7a25214fa0f6b96c5b368eec65218fec826f9
arslene-kabinda <arskabinda@gmail.com>
2022-01-05 17:30:03 +0100

    feat:add beautySalon model

---
+++ models/beautySalon.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const beautySalonSchema = new mongoose.Schema({
+  name: {
+    unique: true,
+    type: String,
+    required: [true, "an hairSalon must have a name"],
+  },
+  description: {
+    type: String,
+    required: [true, "an hairSalon must have  a small description "],
+  },
+  coverImage: {
+    type: String,
+    required: [true, "an hairSalon must have a cover picture"],
+  },
+  adress: {
+    type: [
+      {
+        township: { type: String },
+        quater: { type: String },
+        street: { type: String },
+        number: { type: Number },
+        reference: { type: String },
+      },
+    ],
+  },
+  openingTime: {
+    type: Date,
+    required: [true, "give the opening hour"],
+  },
+  closingTime: {
+    type: Date,
+    required: [true, "give the closing hour"],
+  },
+  map: {
+    type: String,
+  },
+  images: {
+    type: String,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 2,
+  },
+});
+
+const BeautySalon = mongoose.model("BeautySalon", beautySalonSchema);
+
+module.exports = BeautySalon;

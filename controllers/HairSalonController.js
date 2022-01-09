5df5aabe54b99ee1a16c4142a94d6f48cceaa5d6
arslene-kabinda <arskabinda@gmail.com>
2022-01-10 00:19:38 +0100

    feat: add a create crontroller haisalon

--- controllers/HairSalonController.js
+++ controllers/HairSalonController.js
@@ -2,5 +2,27 @@ const HairSalon = require("../models/HairSalonModel");
 
 const APIFeatures = require("../utils/apiFeatures");
 
+exports.aliasTopHairSalons = (req, res, next) => {
+  req.query.limit = "4";
+  req.query.sort = "createdAt";
+  req.query.fields = "name,ratingsAverage";
+  next();
+};
 
-exports
+exports.createHairSalon = async (req, res) => {
+  try {
+    const Body = req.body;
+    const newHairSalon = await HairSalon.create(Body);
+    req.status(201).json({
+      status: "the creation of the hairsalon is done with success",
+      data: {
+        newHairSalon,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};

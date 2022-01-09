f2af7ecd4480ae2dc087c82b76cb65dc1bda04b6
arslene-kabinda <arskabinda@gmail.com>
2022-01-10 00:29:04 +0100

    feat: add getAllHairSalon controller

--- controllers/HairSalonController.js
+++ controllers/HairSalonController.js
@@ -26,3 +26,27 @@ exports.createHairSalon = async (req, res) => {
     });
   }
 };
+
+exports.getAllHairSalon = async (req, res) => {
+  try {
+    const features = new APIFeatures(HairSalon.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const hairSalons = await features.query;
+    //SEND RESPONSE
+    res.status(200).json({
+      status: "success",
+      results: hairSalons.length,
+      data: {
+        hairSalons,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failled",
+      message: err,
+    });
+  }
+};
